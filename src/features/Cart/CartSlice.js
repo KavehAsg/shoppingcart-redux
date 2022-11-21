62256432ea4f10a9b1270d9a55b74a573979caf9
Kaveh Asgari <kaveh.nilkaas@gmail.com>
2022-11-21 20:51:51 +0330

    [bug]: fix actions bug (immer errors)

--- src/features/Cart/CartSlice.js
+++ src/features/Cart/CartSlice.js
@@ -6,16 +6,18 @@ const initialState = {
     totalQuantity: 0,
 };
 
-const sumItemsDetail = (selectedItems) => {
-    const totalQuantity = selectedItems.reduce(
-        (total, item) => total + item.quantity
-        , 0);
-
+const sumPrice = (selectedItems) => {
     const totalPrice = selectedItems.reduce(
         (total, item) => total + item.quantity * item.price
         , 0);
+    return totalPrice;
+}
 
-    return { totalPrice, totalQuantity };
+const sumQuantity = (selectedItems) => {
+    const totalQuantity = selectedItems.reduce(
+        (total, item) => total + item.quantity
+        , 0);
+    return totalQuantity;
 }
 
 export const cartSlice = createSlice({
@@ -23,38 +25,34 @@ export const cartSlice = createSlice({
     initialState,
     reducers: {
         addItem(state, action) {
-            return {
-                selectedItems: [...state.selectedItems, { ...action.payload, quantity: 1 }],
-                ...sumItemsDetail(state.selectedItems)
-            }
+            state.selectedItems = [...state.selectedItems, { ...action.payload, quantity: 1 }];
+            state.totalPrice = sumPrice(state.selectedItems);
+            state.totalQuantity = sumQuantity(state.selectedItems);
         },
         removeItem(state, action) {
             const newSelectedItems = state.selectedItems.filter(item => item.id !== action.payload.id);
             return {
                 ...state,
                 selectedItems: [...newSelectedItems],
-                ...sumItemsDetail(newSelectedItems),
+                totalPrice : sumPrice(newSelectedItems),
+                totalQuantity : sumQuantity(newSelectedItems),
             }
         },
         incrementItem(state, action) {
             const indexI = state.selectedItems.findIndex(
                 (item) => item.id === action.payload.id
             );
-            state.selectedItems[indexI].quantity++;
-            return {
-                ...state,
-                ...sumItemsDetail(state.selectedItems),
-            }
+            state.selectedItems[indexI].quantity += 1;
+            state.totalPrice = sumPrice(state.selectedItems);
+            state.totalQuantity = sumQuantity(state.selectedItems);
         },
         decrementItem(state, action) {
             const indexD = state.selectedItems.findIndex(
                 (item) => item.id === action.payload.id
             );
-            state.selectedItems[indexD].quantity--;
-            return {
-                ...state,
-                ...sumItemsDetail(state.selectedItems),
-            }
+            state.selectedItems[indexD].quantity -= 1;
+            state.totalPrice = sumPrice(state.selectedItems);
+            state.totalQuantity = sumQuantity(state.selectedItems);
         },
         clearAll(state) {
             return {
@@ -68,3 +66,37 @@ export const cartSlice = createSlice({
 
 export const { addItem, removeItem, incrementItem, decrementItem } = cartSlice.actions;
 export default cartSlice.reducer;
+
+/*
+const userReducer = produce((draft, action) => {
+    switch (action.type) {
+        case "renameUser":
+            // OK: we modify the current state
+            draft.users[action.payload.id].name = action.payload.name
+            return draft // same as just 'return'
+        case "loadUsers":
+            // OK: we return an entirely new state
+            return action.payload
+        case "adduser-1":
+            // NOT OK: This doesn't do change the draft nor return a new state!
+            // It doesn't modify the draft (it just redeclares it)
+            // In fact, this just doesn't do anything at all
+            draft = {users: [...draft.users, action.payload]}
+            return
+        case "adduser-2":
+            // NOT OK: modifying draft *and* returning a new state
+            draft.userCount += 1
+            return {users: [...draft.users, action.payload]}
+        case "adduser-3":
+            // OK: returning a new state. But, unnecessary complex and expensive
+            return {
+                userCount: draft.userCount + 1,
+                users: [...draft.users, action.payload]
+            }
+        case "adduser-4":
+            // OK: the immer way
+            draft.userCount += 1
+            draft.users.push(action.payload)
+            return
+    }
+})*/
